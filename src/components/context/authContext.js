4a3130a7d104486a6ff21b6a7ec9019b1255c28d
rahat2020 <kazimdboktiar2020@gmail.com>
2023-10-16 00:58:12 +0600

    issuses fixed

--- src/components/context/authContext.js
+++ src/components/context/authContext.js
@@ -1,78 +1,14 @@
-// "use client"
-// import { createContext, useEffect, useReducer } from "react";
-
-
-// const INITIAL_STATE = {
-//   user: localStorage.getItem("user") || "",
-//   loading: false,
-//   error: null,
-// };
-
-// export const AuthContext = createContext(INITIAL_STATE);
-
-// const AuthReducer = (state, action) => {
-//   switch (action.type) {
-//     case "LOGIN_START":
-//       return {
-//         user: null,
-//         loading: true,
-//         error: null,
-//       };
-//     case "LOGIN_SUCCESS":
-//       return {
-//         user: action.payload,
-//         loading: false,
-//         error: null,
-//       };
-//     case "LOGIN_FAILURE":
-//       return {
-//         user: null,
-//         loading: false,
-//         error: action.payload,
-//       };
-//     case "LOGOUT":
-//       return {
-//         user: null,
-//         loading: false,
-//         error: null,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export const AuthContextProvider = ({ children }) => {
-//   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
-
-//   useEffect(() => {
-//     localStorage.setItem("user", state.user || '');
-//   }, [state.user]);
-
-//   return (
-//     <AuthContext.Provider
-//       value={{
-//         user: state.user,
-//         loading: state.loading,
-//         error: state.error,
-//         dispatch,
-//       }}
-//     >
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-"use client"
-
+"use client";
 import { createContext, useEffect, useReducer } from "react";
 
-const AuthContext = createContext();
 
 const INITIAL_STATE = {
-  user: "",
+  user: localStorage.getItem("user") || "",
   loading: false,
   error: null,
 };
+
+export const AuthContext = createContext(INITIAL_STATE);
 
 const AuthReducer = (state, action) => {
   switch (action.type) {
@@ -105,29 +41,23 @@ const AuthReducer = (state, action) => {
   }
 };
 
-const AuthContextProvider = ({ children }) => {
+export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const savedUser = localStorage.getItem("user");
-      if (savedUser) {
-        dispatch({ type: "LOGIN_SUCCESS", payload: savedUser });
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    if (state.user && typeof window !== "undefined") {
-      localStorage.setItem("user", state.user);
-    }
+    localStorage.setItem("user", state.user || '');
   }, [state.user]);
 
   return (
-    <AuthContext.Provider value={{ ...state, dispatch }}>
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        loading: state.loading,
+        error: state.error,
+        dispatch,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
-
-export { AuthContext, AuthContextProvider };
